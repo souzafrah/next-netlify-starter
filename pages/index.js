3fb6ea9001043f8796cb92c1934f3909f5f48f0e
souzafrah <59568624+souzafrah@users.noreply.github.com>
2022-01-25 13:46:00 -0300

    Update index.js
    CAMBIO DE TITULO + DOMINIO

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,15 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Directora de Arte Jr - Frah Souza</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="'¡Hola, que bueno verte acá!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          Soy Frah Souza, brasileña, directora de arte. <br>
+          Mi nuevo domínio es: <li class="#"> <a href="http://souzafrah.com.ar" target="_blank">souzafrah.com.aR</a></li>
       </main>
 
       <Footer />
